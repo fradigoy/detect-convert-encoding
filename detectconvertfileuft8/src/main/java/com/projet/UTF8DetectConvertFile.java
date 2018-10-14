114914e09ca1f38dafe593e1f9c880ad48639317
fredostar <dev@frederic-radigoy.fr>
2018-10-14 22:47:19 +0200

    tests and fonctions

--- detectconvertfileuft8/src/main/java/com/projet/UTF8DetectConvertFile.java
+++ detectconvertfileuft8/src/main/java/com/projet/UTF8DetectConvertFile.java
@@ -1,15 +1,41 @@
 package com.projet;
 
 
+import org.mozilla.universalchardet.UniversalDetector;
 
-
-
+import java.io.*;
 
 public  class UTF8DetectConvertFile {
 
 
-    public boolean isUTF8() {
+    public boolean isUTF8(File file) throws IOException {
 
-        return true;
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+
+        byte[] buf = new byte[4096];
+        UniversalDetector detector = new UniversalDetector(null);
+
+        int nread;
+        while ((nread = fileInputStream.read(buf)) > 0 && !detector.isDone()) {
+            detector.handleData(buf, 0, nread);
+        }
+
+        detector.dataEnd();
+        // (4)
+        String encoding = detector.getDetectedCharset();
+        // (5)
+
+        detector.reset();
+
+        if(encoding == "UTF-8"){
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
     }
 }
--- detectconvertfileuft8/src/test/java/com/projet/UTF8DetectConvertFileTest.java
+++ detectconvertfileuft8/src/test/java/com/projet/UTF8DetectConvertFileTest.java
@@ -1,18 +1,33 @@
 package com.projet;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class UTF8DetectConvertFileTest {
 
+    private UTF8DetectConvertFile utf8DetectConvertFile;
+
+    private String fileNameNonUtf8 = "document.txt";
+
+    private String fileNameUtf8 = "test-utf8.txt";
+
+
+    @BeforeEach
+    public void avantChaqueTest(){
+         utf8DetectConvertFile = new UTF8DetectConvertFile();
+    }
+
     @Test
-    public void should_returnTrue(){
+    public void should_returnTrue() throws IOException {
 
+        String filename = getClass().getResource(fileNameUtf8).getFile();
 
-        UTF8DetectConvertFile utf8DetectConvertFile = new UTF8DetectConvertFile();
-
-        Assertions.assertTrue(utf8DetectConvertFile.isUTF8());
+        Assertions.assertTrue(utf8DetectConvertFile.isUTF8(new File(filename)));
     }
 
 
